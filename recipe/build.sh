2192175c4cd00531bd0527d710f0d105e8073086
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2019-12-14 01:47:34 -0500

    Initial feedstock commit with conda-smithy 3.6.5.

---
+++ recipe/build.sh
@@ -0,0 +1,9 @@
+set -e
+mkdir -p source/build
+cd source/build
+cmake -DTENSORFLOW_ROOT=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DFLOAT_PREC=${float_prec} ..
+make -j${CPU_COUNT}
+make install
+make lammps
+mkdir -p ${PREFIX}/share
+mv USER-DEEPMD ${PREFIX}/share
