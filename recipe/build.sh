6a898ea809c99623ed28b0cb230a5026b4ee159f
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2020-02-01 11:11:33 -0500

    add --no-as-needed

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 set -e
 mkdir -p source/build
 cd source/build
-cmake -DTENSORFLOW_ROOT=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DFLOAT_PREC=${float_prec} -DCMAKE_CXX_FLAGS="-lrt -pthread" -DCMAKE_SHARED_LINKER_FLAGS_INIT="-lrt -pthread" ..
+cmake -DTENSORFLOW_ROOT=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DFLOAT_PREC=${float_prec} -DCMAKE_CXX_FLAGS="-lrt -pthread" -DCMAKE_SHARED_LINKER_FLAGS_INIT="--no-as-needed -lrt -pthread" ..
 make -j${CPU_COUNT}
 make install
 make lammps
