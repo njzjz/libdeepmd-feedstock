f0b7bf41b6584cd1f88f3ea90fcb033e117d5c62
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-06-13 09:29:24 -0400

    fix deepmodeling/deepmd-kit#730

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,10 @@
 set -e
 mkdir -p source/build
 cd source/build
-cmake -DTENSORFLOW_ROOT=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DFLOAT_PREC=${float_prec} -DCMAKE_CXX_FLAGS="-lrt" -DCMAKE_SHARED_LINKER_FLAGS_INIT="-lrt" ..
+if [ ${dp_variant} == "gpu" ]; then
+    export DEEPMD_CUDA_ARGS="-DUSE_CUDA_TOOLKIT=TRUE"
+fi
+cmake -DTENSORFLOW_ROOT=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DFLOAT_PREC=${float_prec} ${DEEPMD_CUDA_ARGS} ..
 make -j${CPU_COUNT}
 make install
 make lammps
