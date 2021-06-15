220c83c33e979aae1445591a7c8b1479d4d3f606
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-06-15 14:34:03 -0400

    fix script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 set -e
 mkdir -p source/build
 cd source/build
-if [ ${dp_variant} == "gpu" ]; then
+if [[ ${dp_variant} == "gpu" ]]; then
     export DEEPMD_CUDA_ARGS="-DUSE_CUDA_TOOLKIT=TRUE"
 fi
 cmake -DTENSORFLOW_ROOT=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DFLOAT_PREC=${float_prec} ${DEEPMD_CUDA_ARGS} ..
