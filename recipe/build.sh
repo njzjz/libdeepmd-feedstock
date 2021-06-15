0ad8c89eb3780334af14be3ca250cb9853f7cd13
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-06-15 14:36:14 -0400

    fix script

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,11 @@ set -e
 mkdir -p source/build
 cd source/build
 if [[ ${dp_variant} == "gpu" ]]; then
-    export DEEPMD_CUDA_ARGS="-DUSE_CUDA_TOOLKIT=TRUE"
+    DEEPMD_USE_CUDA_TOOLKIT=TRUE
+else
+    DEEPMD_USE_CUDA_TOOLKIT=FALSE
 fi
-cmake -DTENSORFLOW_ROOT=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DFLOAT_PREC=${float_prec} ${DEEPMD_CUDA_ARGS} ..
+cmake -DTENSORFLOW_ROOT=${PREFIX} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DFLOAT_PREC=${float_prec} -DUSE_CUDA_TOOLKIT=${DEEPMD_USE_CUDA_TOOLKIT} ..
 make -j${CPU_COUNT}
 make install
 make lammps
